3fd838d30d8b53f2aa327206db7ea161d8cd354f
Bator Zhigzhitov <b.zhigzhitov@unitpay.ru>
2020-06-29 14:38:45 +0300

    Изучение docker. Первый коммит.

---
+++ api/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { connectDb } = require('./helpers/db');
+const { host, port, db } = require('./configuration/index');
+const app = express();
+
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`Started API service on port: ${port}`);
+        console.log(`Our host: ${host}`);
+        console.log(`Our DataBase: ${db}`);
+    });
+}
+
+app.get("/test", (req, res) => {
+    res.send('Our API server is working correctly.');
+});
+
+connectDb()
+    .on("error", console.log)
+    .on("disconnected", connectDb)
+    .once("open", startServer);
