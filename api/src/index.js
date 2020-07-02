2b9f6ec67fede3ed175d6e4c17348a13b4aa5654
Bator Zhigzhitov <b.zhigzhitov@unitpay.ru>
2020-07-02 13:41:29 +0300

    Изучение docker. Сделан API и Auth сервисы.

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { connectDb } = require('./helpers/db');
 const { host, port, db } = require('./configuration/index');
 const app = express();
+const postSchema = new mongoose.Schema({
+    name: String
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 const startServer = () => {
     app.listen(port, () => {
         console.log(`Started API service on port: ${port}`);
         console.log(`Our host: ${host}`);
         console.log(`Our DataBase: ${db}`);
+
+
+        Post.find(function (err, posts) {
+            if (err) return console.error(err);
+            console.log("posts new", posts);
+        })
+
+
+        /*
+        const silence = new Post({ name: 'Silence' });
+        silence.save(function (err, savedSilence) {
+            if (err) return console.error(err);
+            console.log('savedSilence', savedSilence);
+        });
+        */
     });
 }
 
