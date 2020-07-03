e701f897effa576215520318b3d704c3d1e24dc5
Bator Zhigzhitov <b.zhigzhitov@unitpay.ru>
2020-07-03 11:26:16 +0300

    Production и Development mode для Frontend-сервиса.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Change 111.
+          Edit <code>src/App.js</code> and save to reload. Dran-din-din.
         </p>
         <a
           className="App-link"
